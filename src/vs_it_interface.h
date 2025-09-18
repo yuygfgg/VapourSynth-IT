279c552f67884f7711c20c08ec0f018e4ddd9daf
yuygfgg <140488233+yuygfgg@users.noreply.github.com>
2025-09-18 15:19:15 +0800

    Add meson build system and fixed build for non-x86 cpus.
    Also updated version to 1.2 according to README.md

--- src/vs_it_interface.h
+++ src/vs_it_interface.h
@@ -31,7 +31,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110 - 1301, USA
 #define alignas(x) __declspec(align(x))
 #define ALIGNED_ARRAY(decl, alignment) alignas(alignment) decl
 #else
+#ifndef __C
 #include <x86intrin.h>
+#endif
 #define __forceinline inline
 #define ALIGNED_ARRAY(decl, alignment) __attribute__((aligned(alignment))) decl
 #define _aligned_malloc(decl, alignment) vs_aligned_malloc(alignment, decl)
@@ -40,7 +42,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110 - 1301, USA
 
 #define PARAM_INT(name, def) int name = int64ToIntS(vsapi->propGetInt(in, #name, 0, &err)); if (err) { name = def; }
 
-#define IT_VERSION "0103." "1.1"
+#define IT_VERSION "0103." "1.2"
 
 #include "IScriptEnvironment.h"
 #include "vs_it.h"
